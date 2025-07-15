410ffa6bcb8706e5cf130320852a31cdd76973f2
ElviaChoi <elviachoi924@gmail.com>
2025-07-15 14:26:23 +0900

    tsc 다시 실행

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ console.log(`💻 오늘 부트캠프 수업 있나요? ${isBootcampDay ? "네, 
 let dinnerMenu = null;
 console.log(`🍽️ 저녁 메뉴: ${dinnerMenu !== null && dinnerMenu !== void 0 ? dinnerMenu : "아직 정하지 않았어요."}`);
 // any
-let note = "프로크리에이트로 강아지 그리는 연습함";
+let note = "어제 프로크리에이트로 강아지 그리는 연습함";
 console.log(`📝 오늘의 메모: ${note}`);
 note = ["타입스크립트 복습", "고양이 그리기", "코딩 연습"];
 console.log("📌 오늘 할 일 리스트:", note);
